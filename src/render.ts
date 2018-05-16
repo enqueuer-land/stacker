26c510602f30152693f56a0874140b405217f022
Guilherme Moraes <gmoraes@thoughtworks.com>
2018-05-16 17:31:59 -0300

    Linter is passing

--- src/render.ts
+++ src/render.ts
@@ -1,12 +1,6 @@
 import * as Vue from 'vue'
 import * as App from './components/App.vue'
-// import VueHighlightJS from 'vue-highlightjs'
-import hljs from 'highlight.js'
-
-// Vue.use(VueHighlightJS)
 Vue.config.productionTip = false
 
-
-
-// tslint:disable-next-line:no-unused-expression
+//tslint:disable-next-line:no-unused-expression
 new Vue(App).$mount('#app')
