88df7d01adfa0658d130c76de6670784895919fc
Guilherme Moraes <gmoraes@thoughtworks.com>
2018-05-16 21:34:33 -0300

    lint: making semicolon always needed

--- src/render.ts
+++ src/render.ts
@@ -1,6 +1,6 @@
-import * as Vue from 'vue'
-import * as App from './components/App.vue'
-Vue.config.productionTip = false
+import * as Vue from 'vue';
+import * as App from './components/App.vue';
+Vue.config.productionTip = false;
 
 //tslint:disable-next-line:no-unused-expression
-new Vue(App).$mount('#app')
+new Vue(App).$mount('#app');
