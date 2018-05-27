f625dbefd77861010d2c02c7a26df6d1a31a1819
Guilherme Moraes <gmoraes@thoughtworks.com>
2018-05-27 19:23:00 -0300

    Cleaning some mess and reintegrating with enqueuer

---
+++ src/enqueuer/enqueuer-client.ts
@@ -0,0 +1,88 @@
+// import {Logger} from 'log4js';
+import {spawn, ChildProcess} from 'child_process';
+
+const runnable = {
+    'runnableVersion': '01.00.00',
+    'name': 'runnableHttp',
+    'id': 'randomIdFixedInRunnable',
+    'initialDelay': 0,
+    'runnables': [
+        {
+            'timeout': 30000,
+            'name': 'HttpTitle',
+            'subscriptions': [
+                {
+                    'type': 'http-server',
+                    'name': 'HttpSubscriptionTitle',
+                    'endpoint': '/enqueuer',
+                    'port': 23075,
+                    'method': 'POST',
+                    'response': {
+                        'status': 200
+                    },
+                    'timeout': 10000
+                }
+            ],
+            'startEvent': {
+                'publisher': {
+                    'type': 'http-client',
+                    'name': 'HttpPublisherClientTitle',
+                    'url': 'http://localhost:23075/enqueuer',
+                    'method': 'POST',
+                    'payload': {
+                        'enqueuer': 'virgs'
+                    },
+                    'headers': {
+                        'content-type': 'application/json'
+                    }
+                }
+            }
+        }
+    ]
+};
+
+export type EnqueuerResponseCallback = (response: string) => void;
+
+export class EnqueuerClient {
+    private enqueuer: ChildProcess;
+    private static singleton = new EnqueuerClient();
+    private onEnqueuerResponse: EnqueuerResponseCallback;
+    private constructor() {
+        this.onEnqueuerResponse = () => {};
+        this.startEnqueuer();
+        this.enqueuer.on('exit', (/*statusCode: number*/) => {
+            // Logger.info(`Enqueuer has exited with status: ${statusCode}`);
+            console.log('enqueuer exit');
+            this.startEnqueuer();
+        });
+    }
+
+    public setOnEnqueuerResponse(onEnqueuerResponse: EnqueuerResponseCallback): EnqueuerClient {
+        this.onEnqueuerResponse = onEnqueuerResponse;
+        return this;
+    }
+
+    private startEnqueuer() {
+        // Logger.info(`Starting enqueuer`);
+        this.enqueuer = spawn('node', ['node_modules/enqueuer/js/index.js', '--config-file', 'conf/enqueuer.yml']);
+        console.log("Starting enqueuer");
+        this.enqueuer.stdout.on('data', (data: string) => {
+            console.log('Got enqueuers response');
+            this.onEnqueuerResponse(data);
+            // this.enqueuer.kill('SIGINT');
+        });
+    }
+
+    public static getInstance = () => EnqueuerClient.singleton;
+
+    public run(): void {
+        //TODO: enqueue a message
+        // Logger.debug(`Sending to enqueuer: ${runnable}`
+        //     .substr(0, 100).concat('...'));
+        console.log("Writing message to enqueuer");
+        this.enqueuer.stdin.write(JSON.stringify(runnable) + '\r\n');
+        this.enqueuer.stdin.end();
+        // this.enqueuer.stdin.resume();
+    }
+
+}
--- src/render.ts
+++ src/render.ts
@@ -1,6 +1,11 @@
 import * as Vue from 'vue';
 import * as App from './components/App.vue';
+import { EnqueuerClient } from './enqueuer/enqueuer-client';
 Vue.config.productionTip = false;
 
 //tslint:disable-next-line:no-unused-expression
 new Vue(App).$mount('#app');
+EnqueuerClient.getInstance().setOnEnqueuerResponse((response: string) => {
+        console.log(`Enqueuer response: ${response}`);
+    }).run();
+// EnqueuerClient.getInstance().run();
