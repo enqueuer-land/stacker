879d7d0edb4350296f00905aed3ad6244de1db0b
Guilherme Corsino <gcorsino@thoughtworks.com>
2018-05-30 23:19:03 -0300

    Adds test of App Component

--- src/components/App.test.ts
+++ src/components/App.test.ts
@@ -2,7 +2,8 @@ import { shallowMount } from '@vue/test-utils'
 import App from './App.vue'
 
 describe('App.test.ts', () => {
-    it('loads component as supposed to', () => {
+
+    it('renders component as supposed to', () => {
         const wrapper = shallowMount(App);
         expect(wrapper.find('#main').exists()).toBe(true)
     })
