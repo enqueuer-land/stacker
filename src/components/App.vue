642a6025f60310c7037667acbae235736b53cdc2
Guilherme Moraes <gmoraes@thoughtworks.com>
2018-10-09 18:12:24 -0300

    Fixing CI tests

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,11 +1,15 @@
 <template>
-    <div class="container-fluid">
+    <div id="main" class="container-fluid">
         <div class="row">
             <div class="col-2 nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-                <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Requisition #0</a>
-                <a class="nav-link disabled" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Requisition #1</a>
-                <a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">Requisition #2</a>
-                <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">Requisition #3</a>
+                <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab"
+                   aria-controls="v-pills-home" aria-selected="true">Requisition #0</a>
+                <a class="nav-link disabled" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile"
+                   role="tab" aria-controls="v-pills-profile" aria-selected="false">Requisition #1</a>
+                <a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab"
+                   aria-controls="v-pills-messages" aria-selected="false">Requisition #2</a>
+                <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab"
+                   aria-controls="v-pills-settings" aria-selected="false">Requisition #3</a>
             </div>
 
             <div class="col-10 row">
@@ -13,11 +17,10 @@
                     <Requisition v-model="requisition" v-on:sendClick="sendClick"></Requisition>
                 </div>
                 <div class="col-6">
-
-
-                        <p class="h3 lead">{{this.enqueuerResponse ? this.enqueuerResponse.name: ''}}</p>
-                        <p class="h5" :class="enqueuerResponse ? (enqueuerResponse.valid? 'text-success' : 'text-danger'): ''">{{this.enqueuerResponse ? this.resultMessage: ''}}</p>
-
+                    <p class="h3 lead">{{this.enqueuerResponse ? this.enqueuerResponse.name: ''}}</p>
+                    <p class="h5"
+                       :class="enqueuerResponse ? (enqueuerResponse.valid? 'text-success' : 'text-danger'): ''">
+                        {{this.enqueuerResponse ? this.resultMessage: ''}}</p>
                     <pre><code>{{this.enqueuerResponse}}</code></pre>
                 </div>
             </div>
@@ -27,7 +30,7 @@
 </template>
 
 <script lang="ts">
-    import { EnqueuerClient } from '../enqueuer/enqueuer-client';
+    import {EnqueuerClient} from '../enqueuer/enqueuer-client';
     import * as Requisition from './requisition/Requisition';
     import {RequisitionModel} from "../enqueuer/models/outputs/requisition-model";
     import {TestsCounter} from "../enqueuer/tests-counter";
@@ -42,32 +45,32 @@
                 enqueuerResponse: '',
                 requisition: null,
                 resultMessage: null
-            }
+            };
         },
         methods: {
-            sendClick: function (requisition)  {
+            sendClick: function (requisition) {
                 console.log(`Requisition: ${JSON.stringify(requisition)}`);
 
                 const enqueuer: EnqueuerClient = new EnqueuerClient(requisition);
                 enqueuer.on('response', (response: RequisitionModel) => {
-                        //Removes the stacker requisition layer 'http daemon input' stuff
-                        this.enqueuerResponse = response.requisitions[0];
+                    //Removes the stacker requisition layer 'http daemon input' stuff
+                    this.enqueuerResponse = response.requisitions[0];
 
-                        const testsCounter = new TestsCounter(this.enqueuerResponse);
-                        const testsNumber = testsCounter.getTestsNumber();
-                        let passingTestsNumber = testsNumber - testsCounter.getFailingTestsNumber();
-                        let percentage = testsCounter.getPercentage();
-                        this.resultMessage = `${passingTestsNumber}/${testsNumber} - (${percentage}%) - ${this.enqueuerResponse.time.totalTime}ms`;
+                    const testsCounter = new TestsCounter(this.enqueuerResponse);
+                    const testsNumber = testsCounter.getTestsNumber();
+                    let passingTestsNumber = testsNumber - testsCounter.getFailingTestsNumber();
+                    let percentage = testsCounter.getPercentage();
+                    this.resultMessage = `${passingTestsNumber}/${testsNumber} - (${percentage}%) - ${this.enqueuerResponse.time.totalTime}ms`;
                 });
                 enqueuer.on('exit', (response: number) => console.log(`exit: ${response}`));
                 enqueuer.on('error', (response: Error) => console.error(`error: ${response}`));
 //                enqueuer.on('log', (response: Error) => console.log(`log: ${response}`));
                 enqueuer.send().then((success) => {
-                    console.log(`Sending result: ${success}`)
+                    console.log(`Sending result: ${success}`);
                 });
             }
         }
-    }
+    };
 </script>
 
 <style lang="css">
@@ -78,6 +81,7 @@
         padding-left: 0.6em;
         border-radius: 10rem;
     }
+
     .amqp {
         color: #ffffff;
         background-color: #1d9427;
--- src/components/requisition/event/Assertion.vue
+++ src/components/requisition/event/Assertion.vue
@@ -1,55 +1,28 @@
 <template>
-        <div >
-
-
-                <div class="input-group">
-                    <button class="btn btn-outline-secondary dropdown-toggle input-group-append" type="button" data-toggle="dropdown"
-                            aria-haspopup="true" aria-expanded="false">{{currentExpectation}}</button>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" v-for="(item) of Object.keys(available)" v-on:click="setExpectation(item)">{{item}}</a>
-                    </div>
-                    <input type="text" v-model="firstValue" class="form-control">
-                </div>
-
-
-                <div v-if="availableCriteria" class="input-group">
-                    <button class="btn btn-outline-secondary dropdown-toggle input-group-append" type="button" data-toggle="dropdown"
-                            aria-haspopup="true" aria-expanded="false">{{currentCriterium}}</button>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" v-for="(item) of availableCriteria" v-on:click="setCriteria(item)">{{item}}</a>
-                    </div>
-                    <input type="text" v-model="secondValue" class="form-control">
-                </div>
-
-
-
-            <!--<div class="col-6 row">-->
-
-                <!--<button class="btn btn-outline-secondary dropdown-toggle col-4" type="button" data-toggle="dropdown"-->
-                        <!--aria-haspopup="true" aria-expanded="false">{{currentExpectation}}-->
-                <!--</button>-->
-                <!--<a class="dropdown-menu">-->
-                    <!--<a class="dropdown-item" v-for="(item) of Object.keys(available)"-->
-                       <!--v-on:click="setExpectation(item)">{{item}}</a>-->
-                <!--</a>-->
-                <!--<input class="col-8" label="value" v-model="firstValue" type="text"-->
-                       <!--aria-label="Text input with dropdown button">-->
-
-            <!--</div>-->
-            <!--<div v-if="availableCriteria" class="input-group-append col-6 row">-->
-                <!--<button class="btn btn-outline-secondary dropdown-toggle col-4" type="button" data-toggle="dropdown"-->
-                        <!--aria-haspopup="true" aria-expanded="false">{{currentCriterium}}-->
-                <!--</button>-->
-
-
-                <!--<a class="dropdown-menu">-->
-                    <!--<a class="dropdown-item" v-for="(item) of availableCriteria"-->
-                       <!--v-on:click="setCriteria(item)">{{item}}</a>-->
-                <!--</a>-->
-                <!--<input class="col-8" label="value" v-model="secondValue" type="text"-->
-                       <!--aria-label="Text input with dropdown button">-->
-            <!--</div>-->
+    <div>
+        <div class="input-group">
+            <button class="btn btn-outline-secondary dropdown-toggle input-group-append" type="button"
+                    data-toggle="dropdown"
+                    aria-haspopup="true" aria-expanded="false">{{currentExpectation}}
+            </button>
+            <div class="dropdown-menu">
+                <a class="dropdown-item" v-for="(item) of Object.keys(available)" v-on:click="setExpectation(item)">{{item}}</a>
+            </div>
+            <input type="text" v-model="firstValue" class="form-control">
         </div>
+
+
+        <div v-if="availableCriteria" class="input-group">
+            <button class="btn btn-outline-secondary dropdown-toggle input-group-append" type="button"
+                    data-toggle="dropdown"
+                    aria-haspopup="true" aria-expanded="false">{{currentCriterium}}
+            </button>
+            <div class="dropdown-menu">
+                <a class="dropdown-item" v-for="(item) of availableCriteria" v-on:click="setCriteria(item)">{{item}}</a>
+            </div>
+            <input type="text" v-model="secondValue" class="form-control">
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
