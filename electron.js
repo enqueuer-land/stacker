3dd6157b58df7d25f98bd456d61472a40b3c87d4
hscorrea <habyte@gmail.com>
2018-10-31 17:17:21 -0300

    Add electron and dev-tools

---
+++ electron.js
@@ -0,0 +1,23 @@
+const { default: installExtension, VUEJS_DEVTOOLS } = require('electron-devtools-installer');
+
+const electron = require('electron')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+
+let url
+if (process.env.NODE_ENV === 'DEV') {
+  url = 'http://localhost:8080/'
+} else {
+  url = `file://${process.cwd()}/dist/index.html`
+}
+
+app.on('ready', () => {
+  let window = new BrowserWindow({width: 800, height: 600})
+  window.loadURL(url)
+
+  installExtension(VUEJS_DEVTOOLS)
+  .then((name) => console.log(`Added Extension:  ${name}`))
+  .catch((err) => console.log('An error occurred: ', err));
+
+  window.webContents.openDevTools({ mode: 'bottom' });
+})
