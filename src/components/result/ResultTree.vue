56fca71c27d7a16f3afe7418c1559877182e3bee
Guilherme Moraes <gmoraes@thoughtworks.com>
2018-11-05 18:37:28 -0200

    Removing unnecessary classes

--- src/components/result/ResultTree.vue
+++ src/components/result/ResultTree.vue
@@ -1,21 +1,18 @@
 <template>
     <div class="result-tree">
         <ul class="list-unstyled">
-            <!--<result-requisition :requisition="$store.state.result"/>-->
             <result-deep-tests :node="$store.state.result"/>
         </ul>
     </div>
 </template>
 
 <script>
-    // import ResultRequisition from './ResultRequisition';
     import ResultDeepTests from "./ResultDeepTests";
 
     export default {
         name: 'ResultTree',
         components: {
-            ResultDeepTests,
-            // ResultRequisition
+            ResultDeepTests
         }
     }
 </script>
