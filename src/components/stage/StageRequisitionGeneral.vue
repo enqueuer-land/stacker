b24557d7417bbac24d1b86e3d1c27ab0f81492cd
Guilherme Moraes <gmoraes@thoughtworks.com>
2018-11-09 17:00:07 -0200

    Requisition general tab relayout

--- src/components/stage/StageRequisitionGeneral.vue
+++ src/components/stage/StageRequisitionGeneral.vue
@@ -1,42 +1,55 @@
 <template>
     <div class="stage-requisition-general container-fluid">
         <div class="row">
-            <div class="pl-3 pt-2" style="font-size: 0.8em; color: white">
-                Time out
-            </div>
-        </div>
-        <div class="row">
-            <div class="input-group input-group-sm mb-1 pl-3 pr-3">
-                <input v-model="getComponent().timeout" type="text" class="form-control" style="background-color: transparent; color: white"
-                       placeholder="5000">
-                <div class="input-group-append">
-                    <span class="input-group-text">ms</span>
+            <div class="col">
+                <div class="row">
+                    <div class="pl-3 pt-2" style="font-size: 0.8em; color: white">
+                        Time out
+                    </div>
                 </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="pl-3 pt-2" style="font-size: 0.8em; color: white">
-                Delay
-            </div>
-        </div>
-        <div class="row">
-            <div class="input-group input-group-sm mb-1 pl-3 pr-3">
-                <input v-model="getComponent().delay" type="text" class="form-control" style="background-color: transparent; color: white"
-                       placeholder="0">
-                <div class="input-group-append">
-                    <span class="input-group-text">ms</span>
+                <div class="row">
+                    <div class="input-group input-group-sm mb-1 pl-3 pr-3">
+                        <input v-model="getComponent().timeout" type="text" class="form-control"
+                               style="background-color: transparent; color: white"
+                               placeholder="5000">
+                        <div class="input-group-append">
+                            <span class="input-group-text">ms</span>
+                        </div>
+                    </div>
                 </div>
+
             </div>
-        </div>
-        <div class="row">
-            <div class="pl-3 pt-2" style="font-size: 0.8em; color: white">
-                Iterations
+            <div class="col">
+                <div class="row">
+                    <div class="pl-3 pt-2" style="font-size: 0.8em; color: white">
+                        Delay
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-group input-group-sm mb-1 pl-3 pr-3">
+                        <input v-model="getComponent().delay" type="text" class="form-control"
+                               style="background-color: transparent; color: white"
+                               placeholder="0">
+                        <div class="input-group-append">
+                            <span class="input-group-text">ms</span>
+                        </div>
+                    </div>
+                </div>
+
             </div>
-        </div>
-        <div class="row">
-            <div class="input-group input-group-sm mb-1 pl-3 pr-3">
-                <input v-model="getComponent().iterations" type="text" class="form-control" style="background-color: transparent; color: white"
-                       placeholder="1">
+            <div class="col">
+                <div class="row">
+                    <div class="pl-3 pt-2" style="font-size: 0.8em; color: white">
+                        Iterations
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-group input-group-sm mb-1 pl-3 pr-3">
+                        <input v-model="getComponent().iterations" type="text" class="form-control"
+                               style="background-color: transparent; color: white"
+                               placeholder="1">
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -47,11 +60,10 @@
     export default {
         name: 'StageRequisitionGeneral',
         data: function () {
-            return {
-            }
+            return {}
         },
         methods: {
-            getComponent: function() {
+            getComponent: function () {
                 return this.$store.state.selectedItem
             }
         },
