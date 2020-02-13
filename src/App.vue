e091947d6cdde7ac51ed6ea1f491addcd1a602bb
Guilherme Moraes <guilherme.moraes@ifood.com.br>
2020-02-13 01:01:58 -0300

    Add split.js

--- src/App.vue
+++ src/App.vue
@@ -7,17 +7,39 @@
                         Side bar
                     </div>
                 </b-col>
-                <b-col cols="6">
+                <b-col cols="3">
                     Stage
                     <router-view/>
                 </b-col>
-                <b-col cols="3">
-                    Result
+                <b-col cols>
+                    <div class="wrapper">
+                        <div class="panel panel__left" id="left">
+                            <h1>LEFT</h1>
+                        </div>
+                        <div class="panel panel__right" id="right">
+                            <h1>RIGHT</h1>
+                        </div>
+                    </div>
                 </b-col>
             </b-row>
         </b-container>
     </div>
 </template>
+<script>
+    import split from 'split.js'
+
+    export default {
+        mounted() {
+            split(['#left', '#right'], {
+                sizes: [50, 50],
+                minSize: [100, 300],
+                gutterSize: 2,
+
+            });
+        }
+    };
+
+</script>
 
 <style>
     :root {
@@ -51,5 +73,22 @@
         text-align: center;
         color: #2c3e50;
     }
+
+    .wrapper {
+        display: flex;
+    }
+
+    .panel {
+        height: calc(100vh - 20px);
+    }
+
+    #left {
+        background: red;
+    }
+
+    #right {
+        background: blue;
+    }
+
 
 </style>
