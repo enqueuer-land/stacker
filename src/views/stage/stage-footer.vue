d0b00d9d0d50bd827576a475ed9ece4c3f00d307
Guilherme Moraes <guilherme.moraes@ifood.com.br>
2020-02-17 21:08:44 -0300

    Create stage-footer.vue

---
+++ src/views/stage/stage-footer.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="stage-footer">
+    </div>
+</template>
+<script>
+    import Vue from 'vue';
+
+    export default Vue.extend({
+        name: 'StageFooter'
+    });
+</script>
+<style type="text/css" scoped>
+    #stage-footer {
+        height: 100%;
+        background-color: var(--carabina-body-background-darker-color);
+        border-top: 1px solid var(--carabina-header-background-lighter-color);
+    }
+</style>
