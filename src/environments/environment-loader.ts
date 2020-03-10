81ff2dcfce50200f1adc8b3321501de825827241
Guilherme Moraes <guilherme.moraes@ifood.com.br>
2020-03-10 18:46:31 -0300

    Remove openDirectory option when loading environments

--- src/environments/environment-loader.ts
+++ src/environments/environment-loader.ts
@@ -1,5 +1,4 @@
 import * as fs from "fs";
-import * as path from "path";
 import * as yaml from "yamljs";
 import {remote} from "electron";
 import {IdCreator} from "@/components/id-creator";
@@ -9,27 +8,15 @@ import store from "@/store";
 export class EnvironmentLoader {
 
     public load(): any {
-        return (remote.dialog.showOpenDialogSync({properties: ['openFile', 'openDirectory', 'multiSelections']}) || [])
-            .map((file: string) => EnvironmentLoader.load(file))
+        return (remote.dialog.showOpenDialogSync({properties: ['openFile', 'multiSelections']}) || [])
+            .map((file: string) => EnvironmentLoader.loadFile(file))
             .filter((file: any) => file);
     }
 
     public static importPostmanEnvironment(): void {
-        (remote.dialog.showOpenDialogSync({properties: ['openFile', 'openDirectory', 'multiSelections']}) || [])
+        (remote.dialog.showOpenDialogSync({properties: ['openFile', 'multiSelections']}) || [])
             .map((file: string) => EnvironmentLoader.loadPostmanEnvironment(file))
             .forEach(environment => store.commit('nav-bar/addEnvironment', environment));
-    }
-
-    private static load(file: string): any {
-        const stats = fs.statSync(file);
-        if (stats.isDirectory()) {
-            return EnvironmentLoader.loadDirectory(file);
-        }
-        return EnvironmentLoader.loadFile(file);
-    }
-
-    private static loadDirectory(dirname: string): any {
-        return fs.readdirSync(dirname).map(file => EnvironmentLoader.loadFile(path.join(dirname, file)));
     }
 
     private static loadFile(file: string): any {
